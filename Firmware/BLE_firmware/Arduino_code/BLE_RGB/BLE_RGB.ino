f7625c6bfc147bcf0c062cb962d21a7a97a620f8
Siying Yu <nekosuki326@gmail.com>
2021-10-26 19:34:47 -0400

    Edited Jesse's code, able to read color hex from mobil app now

--- Firmware/BLE_firmware/Arduino_code/BLE_RGB/BLE_RGB.ino
+++ Firmware/BLE_firmware/Arduino_code/BLE_RGB/BLE_RGB.ino
@@ -12,10 +12,19 @@
 #define colorSaturation 255
 #define MAX_DATA_SIZE 128   // Max transmit data size in bit
 
-#define LED_STATE 0x1
-#define LED_COLOR 0x2
-#define LED_ON 0x10
-#define LED_OFF 0x11
+#define LED_STATE 0x01
+#define LED_COLOR 0x02
+#define TEMP_REQ  0x03
+#define TEMP_DATA 0x04
+#define ACC_REQ   0x05
+#define ACC_DATA  0x06
+#define SPK_CMD   0x07
+
+#define LED_OFF   0x10
+#define LED_ON    0x11
+
+#define SPK_OFF   0x70
+#define SPK_ON    0x71
 
 const uint16_t PixelCount = 60; // this example assumes 4 pixels, making it smaller will cause a failure
 const uint8_t PixelPin = 18;  // make sure to set this to the correct pin, ignored for Esp8266
@@ -41,30 +50,50 @@ bool deviceConnected = false; // connection-status flag
 static int commandLength = 0;
 char receivedCommand[MAX_DATA_SIZE] = {}; // commands via bluetooth stored here
 
-void IRAM_ATTR onTimer() {
+BLEServer *pServer = BLEDevice::createServer();
+BLEService *pService = pServer->createService(SERVICE_UUID);  // Create the BLE Service
+BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                        CHARACTERISTIC_UUID_RX,
+                                        BLECharacteristic::PROPERTY_READ|
+                                        BLECharacteristic::PROPERTY_WRITE);
+
+void IRAM_ATTR onTimer() 
+{
   static byte state = LOW;
-  state = ! state;
+  state = !state;
   digitalWrite(LED, state);
 }
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
+class MyServerCallbacks: public BLEServerCallbacks 
+{
+    void onConnect(BLEServer* pServer) 
+    {
       deviceConnected = true;
       Serial.println("connnected to a device!");
     }
-    void onDisconnect(BLEServer* pServer) {
+
+    void onDisconnect(BLEServer* pServer) 
+    {
       deviceConnected = false;
       Serial.println("disconnected!");
+      
+      // Start advertising
+      pServer->getAdvertising()->start();
+      Serial.println("Waiting a client connection to notify...");
     }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
-      commandLength = rxValue.length();
-      if (commandLength > 0) {
-        Serial.println("Received a command\n");
-        strcpy(receivedCommand, rxValue.c_str());
+class MyCallbacks: public BLECharacteristicCallbacks 
+{
+  void onWrite(BLECharacteristic *pCharacteristic) 
+  {
+    std::string rxValue = pCharacteristic->getValue();
+    commandLength = rxValue.length();
+    if (commandLength > 0) 
+    {
+      Serial.println("Received a command\n");
+      strcpy(receivedCommand, rxValue.c_str());
+      
 //        for(int i = 0; i < commandLength; i++) 
 //        {
 //          printf("%x", receivedCommand[i]);
@@ -72,13 +101,13 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 //        printf("\n");
 //        receivedCommand = rxValue.c_str();
 //        printf("%x", receivedCommand);
-        rxValue = "";
-      }
+      rxValue = "";
     }
+  }
 };
 
-void setup() {
- 
+void setup() 
+{
   // this resets all the neopixels to an off state
   strip.Begin();
   strip.Show();
@@ -86,11 +115,7 @@ void setup() {
   BLEDevice::init("this_is_the_fucking_BLE"); // Give it a name
 
   // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
 
   // Create a BLE
   /*BLECharacteristic *pCharacteristic = pService->createCharacteristic(
@@ -98,10 +123,6 @@ void setup() {
                       BLECharacteristic::PROPERTY_NOTIFY
                     );
     pCharacteristic->addDescriptor(new BLE2902());*/
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID_RX,
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
   pCharacteristic->setCallbacks(new MyCallbacks());
 
   // Start the service
@@ -128,6 +149,12 @@ void setup() {
 
 }
 
+// void setup()
+// {
+//   bleInit();
+// }
+
+/*
 void apply_color(unsigned char R, unsigned char G, unsigned char B)
 {
   if (R >= 0 && R <= 255)  ledcWrite(1, R);
@@ -146,8 +173,8 @@ void apply_color(unsigned char R, unsigned char G, unsigned char B)
  Serial.print(B);
 }
 
-void change_color(String cmd) {
-
+void change_color(String cmd) 
+{
   if (cmd.substring(0, 1) == "R")
   {
     //version 5 convert decimal to hex
@@ -175,7 +202,8 @@ void change_color(String cmd) {
   }
 }
 
-void blink_1()  {
+void blink_1()  
+{
   Serial.println("Received a blinking require command!");
   digitalWrite(LED, HIGH);
   delay(500);
@@ -186,6 +214,7 @@ void blink_1()  {
   digitalWrite(LED, LOW);
   delay(500);
 }
+*/
 
 int ledState(byte state)
 {
@@ -220,6 +249,45 @@ int ledColor(byte red, byte green, byte blue)
   return 0;
 }
 
+String createCMD(byte dataType, byte dataSize, byte *cmdData)
+{
+  uint8_t cmd[dataSize + 2];
+  cmd[0] = dataType;
+  cmd[1] = dataSize;
+  for(int i = 0; i < dataSize; i++)
+  {
+    cmd[i + 2] = cmdData[i];
+  }
+  cmd[dataSize + 1] = '\0';
+
+  return cmd;
+}
+
+// Receive data, send data through BLE, return 0 if data transmit success
+int sendData(String cmd)
+{
+  pCharacteristic->setValue(cmd);
+  
+  return 0;
+}
+
+int sendTemp()
+{
+  byte *data = {0x55};
+  printf("Received temp request.\n");
+  sendData(createCMD(TEMP_DATA, 1, data));
+  printf("Temp data sent.\n");
+
+  return 0;
+}
+
+int sendAcc()
+{
+  printf("Received accelerometer request.\n");
+ 
+  return 0;
+}
+
 void readCommand()
 {
   byte cmdType = receivedCommand[0];
@@ -241,7 +309,19 @@ void readCommand()
       ledColor(red, green, blue);
       break;
     }
-    
+
+    case TEMP_REQ:
+    {
+      sendTemp();
+      break;
+    }
+
+    case ACC_REQ:
+    {
+      sendAcc();
+      break;
+    }
+
     default:
       break;      
   }
@@ -253,37 +333,39 @@ void loop() {
   if (commandLength > 0)
   {
     readCommand();
-//    static int cnt = 0;
-//    cnt++;
-////    printf("%d\n", cnt);
-////    printf("%d\n", cnt%2);
-//    delay(100);
-//    printf("This is what I got: ", receivedCommand);
-//    for(int i = 0; i < commandLength; i++) 
-//    {
-//      printf("%x", receivedCommand[i]);
-//    }
-//    printf("\n");
-//    if(cnt%2 == 0)
-//    {
-//    strip.SetPixelColor(0, red);
-//    strip.SetPixelColor(1, red);
-//    strip.SetPixelColor(2, red);
-//    strip.SetPixelColor(3, red);
-//    strip.SetPixelColor(4, red);
-//    strip.Show();
-//    }
-//    
-//    else
-//    {
-//    // turn off the pixels
-//    strip.SetPixelColor(0, black);
-//    strip.SetPixelColor(1, black);
-//    strip.SetPixelColor(2, black);
-//    strip.SetPixelColor(3, black);
-//    strip.SetPixelColor(4, black);
-//    strip.Show();
-//    }
+
+  /*
+   static int cnt = 0;
+   cnt++;
+//    printf("%d\n", cnt);
+//    printf("%d\n", cnt%2);
+   delay(100);
+   printf("This is what I got: ", receivedCommand);
+   for(int i = 0; i < commandLength; i++) 
+   {
+     printf("%x", receivedCommand[i]);
+   }
+   printf("\n");
+   if(cnt%2 == 0)
+   {
+   strip.SetPixelColor(0, red);
+   strip.SetPixelColor(1, red);
+   strip.SetPixelColor(2, red);
+   strip.SetPixelColor(3, red);
+   strip.SetPixelColor(4, red);
+   strip.Show();
+   }
+   
+   else
+   {
+   // turn off the pixels
+   strip.SetPixelColor(0, black);
+   strip.SetPixelColor(1, black);
+   strip.SetPixelColor(2, black);
+   strip.SetPixelColor(3, black);
+   strip.SetPixelColor(4, black);
+   strip.Show();
+   }
     
     if (receivedCommand == "BL1")
     {
@@ -314,8 +396,8 @@ void loop() {
     {
       change_color(receivedCommand);
     }
+*/
 
     commandLength = 0;
   }
-  //delay(10);
 }
